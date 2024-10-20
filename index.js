3a68911458405ddfa613affd91577835f7de9d32
Dhruv Pokhriyal <171908255+DhruvPokhriyal@users.noreply.github.com>
2024-10-20 11:16:05 +0530

    Add the follow-up form for the remove room feature

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const ADMIN_PASSWORD = "admin@123";
 const closeButtons = Array.from(document.querySelectorAll(".close-button"));
 const closeButtonIcons = Array.from(document.querySelectorAll(".btn-close"));
 const signUpForm = Array.from(document.querySelectorAll(".sign-up"));
+const adminRoleButton = Array.from(document.querySelectorAll(".admin-role"));
 for (let i = 0; i < closeButtons.length; i++) {
     closeButtons[i].addEventListener("click", () => {
         signUpForm[i].reset();
@@ -12,27 +13,6 @@ for (let i = 0; i < closeButtons.length; i++) {
         signUpForm[i].reset();
     });
 }
-let email = Array.from(document.querySelectorAll(".mail"));
-let password = Array.from(document.querySelectorAll(".password"));
-
-// document.querySelectorAll(".sign-up").forEach((form) => {
-//     form.addEventListener("submit", (e) => {
-//         e.preventDefault();
-
-//         const mail = form.querySelector(".mail").value;
-//         const pswd = form.querySelector(".password").value;
-
-//         if (mail === "" || pswd === "") {
-//             alert("Ensure you input a value in both fields!");
-//         } else if (mail === ADMIN_MAIL && pswd === ADMIN_PASSWORD) {
-//             alert("Form Successfully Submitted");
-//         } else {
-//             alert("Invalid Info\n");
-//         }
-
-//         form.reset();
-//     });
-// });
 
 for (let i = 0; i < signUpForm.length; i++) {
     let form = signUpForm[i];
@@ -46,6 +26,15 @@ for (let i = 0; i < signUpForm.length; i++) {
         } else if (mail === ADMIN_MAIL && pswd === ADMIN_PASSWORD) {
             alert("Form Successfully Submitted");
             closeButtons[i].click();
+            if (form.id === "sign-up-1") {
+                document.querySelector("#add-room button.admin-role").click();
+            }
+
+            if (form.id === "sign-up-2") {
+                document
+                    .querySelector("#remove-room button.admin-role")
+                    .click();
+            }
         } else {
             alert("Invalid Info\n");
         }
